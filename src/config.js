4567b6e9e7458dd0b1b6659b7884fd955e9ab172
Begzod Muxamedov <bmukhamedov585@bk.ru>
2021-03-12 19:31:23 +0500

    feat: test new methods

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const TOKEN = '1607140454:AAGWPi8lL2dWsMmjpp2bvvIfV68-iyY2F58';
+const host = '';
+const url = 'https://eaf2da221dde.ngrok.io';
+module.exports = {
+	creatorID: 482944903, // @Muxamedov_Begzod
+	adminsIDs: [
+		106529367 // @Begzod_Muxamedov 106529367
+	],
+	channelID: -1001437763483,
+	TOKEN,
+	DBAdress: 'database.json5',
+	port: 80,
+	url,
+}
---
+++ src/index.js
@@ -0,0 +1,50 @@
+const TelegramBot = require('node-telegram-bot-api');
+const request = require('request');
+const config = require('./config');
+const _ = require('./helper');
+
+const bot = new TelegramBot(config.TOKEN, {
+	polling: false,
+	webHook: {
+		host: config.host,
+		port: config.port,
+		autoOpen: false
+	}
+});
+
+bot.openWebHook();
+bot.setWebHook(`${config.url}/bot${config.TOKEN}`);
+
+console.log('\nBot has been Started ...\n');
+
+bot.on('message', msg => {
+	console.log('\n---------- New Message ----------\n');
+	console.log(msg);
+});
+
+bot.on('callback_query', (query) => {
+
+});
+
+bot.onText(/\/help( .+)*/, (msg, [source, match]) => {
+	const { chat: { id }} = msg;
+	bot.sendMessage(id, `Your argument: ${match}`);
+});
+
+// request.post({
+// 	url: `https://api.telegram.org/bot${config.TOKEN}/createChatInviteLink`,
+// 	json: {chat_id: config.channelID}
+// }, (error, response, body) => {
+// 	console.log(body);
+// });
+
+request.post({
+	url: `https://api.telegram.org/bot${config.TOKEN}/editChatInviteLink`,
+	json: {
+		chat_id: config.channelID,
+		invite_link: 'https://t.me/joinchat/mxCMroMG4WRlNzdi',
+		member_limit: 1
+	}
+}, (error, response, body) => {
+	console.log(body);
+});
